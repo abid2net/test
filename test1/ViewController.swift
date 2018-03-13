eadb64fbc23f20daac21835976328d4feceb2d6e
AllianzAndroid <allianzimac@allianzs-imac.local>
2018-03-13 11:13:17 +0530

    commit changes

--- test1/ViewController.swift
+++ test1/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +21,36 @@ class ViewController: UIViewController {
     }
 
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath){
+        let deselectedCell = tableView.cellForRow(at: indexPath as IndexPath)!
+        deselectedCell.backgroundColor = UIColor.clear
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+            print("delete tableview cell")
+        }
+    }
 }
 
